9a0514f3b59449ad61f3531ec97b669521c6c4a6
Ronan Limon Duparcmeur <ronan@2-45.pm>
2022-07-10 10:22:38 +0200

    Experiment: extract the players list to a model
    Instead of being a simple data collection provided by the Game model,
    the list of players ordered for a given player is now a self-contained
    model.
    
    On one hand, this allows the leverage of Rails' conventions (something
    visible in the simplification of the call to
    `#broadcast_replace_later_to`), and technically enforces the Single
    Responsibility Principle, by reducing the scope of the Game model.
    
    On the other hand, the end result is a bit clunky – in particular the
    way the `PlayersList` model has to implement `#id` and `.find` so
    that it can be serialized to a GlobalID and passed to ActiveJob. And the
    business logic behind this list is so simple that extracting it from
    the `Game` model doesn't seem that important.
    
    I want to keep this approach for future reference, but not in the
    definitive codebase – this commit will be reverted immediately.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,5 @@
 class Game < ApplicationRecord
   has_many :players, after_add: :refesh_players_list, after_remove: :refesh_players_list do
-    def ordered_for(player)
-      order(:position)
-        .to_a
-        .then { |ps| ps.rotate!(ps.index(player) - (ps.size/2.0).ceil + 1) }
-    end
-    
     def register(player_attributes)
       create(player_attributes.merge(position: maximum(:position).to_i + 1))
     end
@@ -15,7 +9,7 @@ class Game < ApplicationRecord
   
   def refesh_players_list(new_player)
     players.excluding(new_player).each do |player|
-      broadcast_replace_later_to player, partial: "games/players", locals: { game: self, player: player }, target: "players"
+      PlayersList.new(game: self, current_player: player).broadcast_replace_later_to(player)
     end
   end
 end
