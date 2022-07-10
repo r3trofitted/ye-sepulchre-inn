9a383be4a25938d81bf43e9b02b4f65a38ceb029
Ronan Limon Duparcmeur <ronan@2-45.pm>
2022-07-10 10:24:12 +0200

    Return the players list to the `Game` model
    Reverts "Experiment: extract the players list to a model", which was
    not meant to be kept anyway.
    
    (This reverts commit 660fcb960b1e8d2aacf9929df71cda3219f6472e.)

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,11 @@
 class Game < ApplicationRecord
   has_many :players, after_add: :refesh_players_list, after_remove: :refesh_players_list do
+    def ordered_for(player)
+      order(:position)
+        .to_a
+        .then { |ps| ps.rotate!(ps.index(player) - (ps.size/2.0).ceil + 1) }
+    end
+    
     def register(player_attributes)
       create(player_attributes.merge(position: maximum(:position).to_i + 1))
     end
@@ -9,7 +15,7 @@ class Game < ApplicationRecord
   
   def refesh_players_list(new_player)
     players.excluding(new_player).each do |player|
-      PlayersList.new(game: self, current_player: player).broadcast_replace_later_to(player)
+      broadcast_replace_later_to player, partial: "games/players", locals: { game: self, player: player }, target: "players"
     end
   end
 end
